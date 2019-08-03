2cc1e3e749a0216c1145b8e4370803b1f88e8083
Edwin <46379855+Ebi2626@users.noreply.github.com>
2019-08-03 21:49:37 +0200

    Add files via upload
    It' s version which can show position of the mouse and give us window size. Nevertheless it still need to correct showing position of the mouse, because it show it based at document height - not window. Its also need to change specific color changer into another random color changer.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,91 @@
 import "../sass/style.scss";
+
+function toggle () {
+const wrapper = document.getElementById('wrapper');
+if (wrapper.classList.contains("nav__wrapper--active")) {
+    slideEffect(); 
+}
+else {;
+    slideEffect();
+}
+}
+function listeners () {
+    const menu = document.getElementById('hamburger');
+    menu.addEventListener('click', toggle, true);
+}
+
+function pageBlock() {
+    const wrapper = document.getElementById('wrapper');
+    if (wrapper.classList.contains("nav__wrapper--active")) {
+        document.body.style.overflowY = "hidden";
+    } else {
+        document.body.style.overflowY = "auto";
+    }
+}
+function slideEffect() {
+    const wrapper = document.getElementById('wrapper');
+    if (wrapper.style.right === "-100vw" || !(wrapper.classList.contains('nav__wrapper--active'))){
+        wrapper.classList.add("nav__wrapper--active");
+       setTimeout(function() {
+        wrapper.style.right = "0px";
+       }, 301);
+      setTimeout(pageBlock, 302);
+      
+    } else {
+            wrapper.style.right = "-100vw";
+           setTimeout(function() {
+           wrapper.classList.remove("nav__wrapper--active");
+           }, 301);
+           setTimeout(pageBlock, 302);
+    }
+}
+function changingColours() {
+    let elipse = document.querySelectorAll(".elipse");
+    setInterval(function(){
+        for (let i = 0, max = elipse.length - 1; max; i++){
+            let name = elipse[i].id;
+            let item = document.getElementById(name);
+            let x = item.classList.contains("azure");
+            let y = item.classList.contains("lime");
+            let z = item.classList.contains("navyBlue");
+            if (x) {
+                item.classList.remove("azure");
+                item.classList.add("lime");
+            } else if (y) {
+                item.classList.remove("lime");
+                item.classList.add("navyBlue");
+            } else if (z) {
+                item.classList.remove("navyBlue");
+                item.classList.add("azure");
+            } else {
+                return
+            }
+        };   
+    }, 2500);   
+}
+
+
+
+function mouseParallax() {
+    window.addEventListener('mousemove', function(e) {
+    let x = e.pageX,
+     y= e.pageY,
+     screenx = window.innerWidth,
+     screeny = window.innerHeight,
+     x0 = screenx/2,
+     y0 = screeny/2;
+     window.onclick = function() {
+         console.log("x: " + x + ", y: " + y + ", screenx: " + screenx + ", screeny: " + screeny + ", x0: " + x0+ ", y0: " + y0);
+     }
+
+    });
+};
+
+
+
+
+
+
+window.addEventListener('DOMContentLoaded', listeners);
+window.addEventListener('DOMContentLoaded', changingColours);
+window.addEventListener('mousemove', mouseParallax);
