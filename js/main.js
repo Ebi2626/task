543d3b9b98199de55588b3f815937ffa507e4289
Edwin <46379855+Ebi2626@users.noreply.github.com>
2019-08-07 21:11:08 +0200

    Still unfinished
    It's still needs to be done: 
    - popup
    - fix mechanism work with cookies
    - refactoring
    - build
    - tiny images

--- js/main.js
+++ js/main.js
@@ -5,17 +5,17 @@ smoothscroll.polyfill();
 function headImgChanger () {
     if (window.innerWidth >= 1024) {
         document.getElementById('header').src = "/img/header-img-desktop.png";
+        document.getElementById('logo').src = "/img/logo-desktop.png"
     } else {
         document.getElementById('header').src = "/img/header-img.png";
     };
 };
-
 function toggle () {
 const wrapper = document.getElementById('wrapper');
 if (wrapper.classList.contains("nav__wrapper--active")) {
     slideEffect(); 
 }
-else {;
+else {
     slideEffect();
 }
 }
@@ -26,13 +26,15 @@ function listeners () {
     if (window.innerWidth < 470){
     menuItem.forEach(function (e) {
         e.addEventListener('click', toggle);
-        console.log(e);
     });
+} else if (window.innerWidth >= 1024) {
+    window.addEventListener('mousemove', mouseParallax);
 } else {
+    window.removeEventListener('mousemove', mouseParallax);
+    window.removeEventListener('mousemove', paralaksa);
     return
 };
 };
-
 function pageBlock() {
     const wrapper = document.getElementById('wrapper');
     if (wrapper.classList.contains("nav__wrapper--active")) {
@@ -50,14 +52,12 @@ function slideEffect() {
         wrapper.style.right = "0px";
        }, 301);
       setTimeout(pageBlock, 302);
-      
     } else {
             wrapper.style.right = "-100vw";
            setTimeout(function() {
            wrapper.classList.remove("nav__wrapper--active");
            }, 301);
            setTimeout(pageBlock, 302);
-    
         }
     } else {
         return
@@ -83,45 +83,66 @@ function changingColours() {
                 item.classList.add("azure");
             } else {
                 return
-            }
-        };   
+            } 
+        }; 
+        cookie('Elipsy');  
     }, 2500);   
 }
-
-
-
 function mouseParallax() {
    let screenx = window.innerWidth,
     screeny = window.innerHeight;
-
-
-        window.addEventListener('mousemove', function(e) {
+    if (screenx < 1024){
+        window.removeEventListener('mousemove', paralaksa);
+    } else {
+        window.addEventListener('mousemove', function paralaksa(e) {
             let x = e.clientX,
-             y= e.clientY,
+             y = e.clientY,
              poziom = x / screenx,
              pion = y / screeny,
              x0 = screenx/2,
              y0 = screeny/2,
              right = x - x0,
              left = x0 - x,
+             top = y - y0,
+             bottom = y0 - y,
              moveX,
              moveY;
-             if (poziom > .5) {
-                moveX = right/x0;
+             /* Mouse on right side */
+             if ((poziom > .5) && (pion > .5)) {
+                moveX = -(right/x0);
+                moveY = -(top/y0);
                 document.querySelectorAll(".elipse").forEach(function(el){
                     el = el.id;
-                    document.getElementById(el).style.transform = "translateX("+Math.round(moveX*10)+"px)";
-                })
-            } else {
-                moveX = left/x0;
-                document.querySelectorAll(".elipse").forEach(function(el){
-                    el = el.id;
-                    document.getElementById(el).style.transform = "translateX("+Math.round(-moveX*10)+"px)";
+                    document.getElementById(el).style.transform = "translate("+Math.round(moveX*10)+"px, "+Math.round(moveY*10)+"px)";
                 });
-            };
-            });
+            } else if ((poziom > .5) && (pion < .5)) {
+                moveX = -(right/x0);
+                moveY = bottom/y0;
+                document.querySelectorAll(".elipse").forEach(function(el){
+                    el = el.id;
+                    document.getElementById(el).style.transform = "translate("+Math.round(moveX*10)+"px, "+Math.round(moveY*10)+"px)";
+                });
+            /* Mouse on left side */
+            } else if ((poziom < .5) && (pion < .5)) {
+                moveX = left/x0;
+                moveY = bottom/y0;
+                document.querySelectorAll(".elipse").forEach(function(el){
+                    el = el.id;
+                    document.getElementById(el).style.transform = "translate("+Math.round(moveX*10)+"px, "+Math.round(moveY*10)+"px)";
+                });
+            } else if ((poziom < .5) && (pion > .5)) {
+                moveX = left/x0;
+                moveY = -(top/y0);
+                document.querySelectorAll(".elipse").forEach(function(el){
+                    el = el.id;
+                    document.getElementById(el).style.transform = "translate("+Math.round(moveX*10)+"px, "+Math.round(moveY*10)+"px)";
+                });
+            } else {
+                console.log("środek ekranu");
+            }
+        });
+    };
 };
-
 function navback() {
     if (window.innerWidth > 470) {
         document.getElementById("wrapper").classList.remove("nav__wrapper--active");
@@ -130,14 +151,53 @@ function navback() {
         return
     }
 };
+function cookie (name) {
+    console.log("działa");
+    let colour = [];
+    document.querySelectorAll('.elipse').forEach(function(el){
+        let klasa = document.getElementById(el.id).classList.value;
+        colour.push(klasa);
+       return colour;
+    });
+    console.log(colour);
+    const cookieName = encodeURIComponent(name);
+    const cookieVal = encodeURIComponent(colour);
+    document.cookie = cookieName+"=" + cookieVal;
+}
+function setColours() {
+    if (document.cookie.con)
+    document.querySelectorAll('.elipse').forEach(function(e){
+        let el = e.id;
+        let classArray = showCookie('Elipsy', e).replace(",", " ");
+        document.getElementById(el).className = classArray;
+        console.log(e.classArray);
+        console.log(showCookie('Elipsy', 0));
+    })
+}
+function deleteCookie(name) {
+    const cookieName = encodeURIComponent(name);
+    document.cookie = cookieName + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
 
+function showCookie(name) {
+    if (document.cookie != "") {
+        const cookies = document.cookie.split(/; */);
 
-
-
+        for (let i=0; i<cookies.length; i++) {
+            const cookieName = cookies[i].split("=")[0];
+            const cookieVal = cookies[i].split("=")[1];
+            if (cookieName === decodeURIComponent(name)) {
+                return decodeURIComponent(cookieVal[i]);
+            }
+        }
+    }
+}
+window.addEventListener('DOMContentLoaded', setColours)
 window.addEventListener('DOMContentLoaded', changingColours);
 window.addEventListener('DOMContentLoaded', headImgChanger);
-window.addEventListener('mousemove', mouseParallax);
 window.addEventListener('resize', navback);
 window.addEventListener('resize', headImgChanger);
 window.addEventListener('resize', listeners);
 window.addEventListener('DOMContentLoaded', listeners);
+
+
